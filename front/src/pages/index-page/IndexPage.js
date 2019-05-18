4d48173ddd9d56e722c146abba461cd5db3d7478
Konstantin Lobov <Lobov.K.Evg@sberbank.ru>
2019-05-18 19:40:19 +0300

    FRONT: Index page redirect

--- front/src/pages/index-page/IndexPage.js
+++ front/src/pages/index-page/IndexPage.js
@@ -1,13 +1,14 @@
 import React from "react"
 import style from "../challenge-search/ChallengeSearch.module.css"
+import { Button } from "antd"
 
 
 export default () => (
     <div className={style.content}>
 
 
-Kek
 
 
+        <Button href="/main" >Перейти к профилю</Button>
     </div>
 )
