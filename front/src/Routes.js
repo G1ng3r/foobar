c8c77968aae0ff362934ee507221e90aeb7fe1f9
Andrey Vlasov <innoavvlasov@gmail.com>
2019-05-18 20:40:27 +0300

    intro page

--- front/src/Routes.js
+++ front/src/Routes.js
@@ -8,23 +8,28 @@ import ChallengeSelect from "./pages/challenge-select/ChallengeSelect"
 import ChallengeCurrent from "./pages/challenge-current/ChallengeCurrent"
 import ChallengeTake from "./pages/challenge-take/ChallengeTake"
 import IndexPage from "./pages/index-page/IndexPage"
+import Header from "./components/header/Header"
+
+import './App.css';
 
 export default () => (
     <div>
         <BrowserRouter>
             <Switch>
-                <Route path={'/'} exact={true} component={IndexPage}/>
-                <Route path={'/main'} exact={true} component={Main}/>
-                <Route path={'/login'} exact={true} component={Login}/>
-                <Route path={'/challenge/search'} component={ChallengeSearch}/>
-                <Route path={'/challenge/select'} component={ChallengeSelect}/>
-                <Route path={'/challenge/take'} component={ChallengeTake}/>
-                <Route path={'/challenge/current'} component={ChallengeCurrent}/>
-                {/*<Route path={'/challenge'} component={Challenge}/>*/}
-                <Route path={'/settings'} exact={true} component={() => <h1>HUI VAM</h1>}/>
+                <Route path={'/'} exact={true} component={IndexPage} />
+                <div className="App">
+                    <Header/>
+                    <Route path={'/main'} exact={true} component={Main}/>
+                    <Route path={'/login'} exact={true} component={Login}/>
+                    <Route path={'/challenge/search'} component={ChallengeSearch}/>
+                    <Route path={'/challenge/select'} component={ChallengeSelect}/>
+                    <Route path={'/challenge/take'} component={ChallengeTake}/>
+                    <Route path={'/challenge/current'} component={ChallengeCurrent}/>
+                    {/*<Route path={'/challenge'} component={Challenge}/>*/}
+                    <Route path={'/settings'} exact={true} component={() => <h1>HUI VAM</h1>}/>
+                </div>
                 <Redirect to={'/main'}/>
             </Switch>
-            <Route/>
         </BrowserRouter>
     </div>
 )
