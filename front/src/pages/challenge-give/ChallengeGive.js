73bfe9d5dcaf99d505b1b5a84d3f8717ba63587d
Konstantin Lobov <Lobov.K.Evg@sberbank.ru>
2019-05-18 23:26:01 +0300

    FRONT: Give challenge page with mock users

--- front/src/pages/challenge-give/ChallengeGive.js
+++ front/src/pages/challenge-give/ChallengeGive.js
@@ -1,33 +1,35 @@
 import React from "react"
-import { Tabs } from "antd"
+import { Spin, Tabs } from "antd"
 import Friend from "./Friend"
+import axios from "axios"
 
-//FIXME: С бэка
-const friends = [
-    {
-        name: 'Олег Дегтев',
-        avatar: ''
-    },
-    {
-        name: 'Костя Лобов',
-        avatar: ''
-    },
-    {
-        name: 'Тони Старк',
-        avatar: ''
-    },
-    {
-        name: 'Саян Базарсадаев',
-        avatar: ''
-    },
-]
 
 const { TabPane } = Tabs
 
 export default class ChallengeGive extends React.Component {
 
     state = {
-        tab: "1"
+        tab: "1",
+        users: []
+    }
+
+    loadUsers = () => {
+
+        //FIXME: С бэка
+        axios.get('https://randomuser.me/api/?results=50').then((data) => {
+            const users = data.data.results.map((user, idx) => ({
+                name: `${user.name.first} ${user.name.last}`,
+                avatar: user.picture.thumbnail,
+                id: idx
+            }))
+
+            this.setState({ users })
+        })
+
+    }
+
+    componentWillMount() {
+        this.loadUsers()
     }
 
     changeTab = (tab) => {
@@ -35,21 +37,26 @@ export default class ChallengeGive extends React.Component {
     }
 
     render() {
+
+
+        if(this.state.users.length === 0) {
+            return <Spin style={{display: 'flex', justifyContent: 'center'}} size="large" />
+        }
         return(
             <React.Fragment>
                 <Tabs style={{ textAlign: 'center'}} defaultActiveKey="1" onChange={this.changeTab}>
                     <TabPane tab="Друзья" key="1">
-                        {friends.map(friend => (
+                        {this.state.users.slice(0, 16).map(friend => (
                             <Friend key={friend.name} friend={friend} />
                         ))}
                     </TabPane>
                     <TabPane tab="Рядом" key="2">
-                        {friends.map(friend => (
+                        {this.state.users.slice(16, 25).map(friend => (
                             <Friend key={friend.name} friend={friend} />
                         ))}
                     </TabPane>
                     <TabPane tab="Все" key="3">
-                        {friends.map(friend => (
+                        {this.state.users.slice(25).map(friend => (
                             <Friend key={friend.name} friend={friend} />
                         ))}
                     </TabPane>
