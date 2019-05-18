53f01cbf8fd6435ca06514882c75203e061f198a
Konstantin Lobov <Lobov.K.Evg@sberbank.ru>
2019-05-18 20:18:57 +0300

    FRONT: Upload files UI

--- front/src/pages/challenge-current/ChallengeCurrent.js
+++ front/src/pages/challenge-current/ChallengeCurrent.js
@@ -1,25 +1,61 @@
 import React from "react"
-import { Affix, Button, Col, Row, Typography } from "antd"
+import { Affix, Button, Col, Modal, Row, Typography } from "antd"
 import cn from 'classnames'
 
 import style from './ChallengeCurrent.module.css'
 import Challenge from "./Challenge"
+import FileUpload from "./FileUpload"
 
-export default () => (
-    <React.Fragment>
-        <div className={style.content}>
-            <Typography.Title level={4}>Ваше текущее задание</Typography.Title>
-            <Challenge/>
-        </div>
-        <Affix offsetBottom={32} className={style["action-bar"]}>
-            <Row gutter={40}>
-                <Col span={12}>
-                    <Button className={cn(style.button, style.red)} size="large">Отказаться</Button>
-                </Col>
-                <Col span={12}>
-                    <Button className={cn(style.button, style.green)} size="large">Готово!</Button>
-                </Col>
-            </Row>
-        </Affix>
-    </React.Fragment>
-)
+class ChallengeCurrent extends React.Component {
+
+    state = {
+        visible: false
+    }
+
+    handleDoneClick = () => {
+        this.setState({
+            visible: true
+        })
+    }
+
+    handleCancel = () => {
+        this.setState({
+            visible: false
+        })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className={style.content}>
+                    <Typography.Title level={4}>Ваше текущее задание</Typography.Title>
+                    <Challenge/>
+                </div>
+                <Affix offsetBottom={32} className={style["action-bar"]}>
+                    <Row gutter={40}>
+                        <Col span={12}>
+                            <Button href="/main" className={cn(style.button, style.red)}
+                                    size="large">Отказаться</Button>
+                        </Col>
+                        <Col span={12}>
+                            <Button className={cn(style.button, style.green)} size="large" onClick={this.handleDoneClick}>Готово!</Button>
+                        </Col>
+                    </Row>
+                </Affix>
+
+                <Modal
+                    title="Сфотографируй результат"
+                    visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                    cancelText={"Отмена"}
+                    okText={"Загрузить"}
+                >
+                    <FileUpload/>
+                </Modal>
+            </React.Fragment>
+        )
+    }
+}
+
+export default ChallengeCurrent
