00c64ca1d36c5c283ac9de81a7f7a340cdeb186d
Konstantin Lobov <Lobov.K.Evg@sberbank.ru>
2019-05-18 13:43:11 +0300

    FRONT: Main page and Header basic layout

--- front/src/pages/main/Main.js
+++ front/src/pages/main/Main.js
@@ -1,6 +1,46 @@
 import React from "react"
+import { Affix, Button } from "antd"
 
+import style from './Main.module.css'
 
 export default () => (
-    <h1>Main page</h1>
+    <React.Fragment>
+        <div className={style.content}>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+            <h1>Main page</h1>
+        </div>
+        <Affix offsetBottom={32} className={style["action-bar"]}>
+            <Button  type="primary" shape="round" size={'large'} onClick={() => { /* TODO */ }} >
+                Do it now!
+            </Button>
+        </Affix>
+    </React.Fragment>
 )
