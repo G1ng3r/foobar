9afdfbbdf333662bf772ffa7f69d0fe03bc1fe3b
Sayan Bazarsadaev <s.bazarsadaev@tinkoff.ru>
2019-05-19 08:12:55 +0300

    fix challenge types dao

--- back/src/main/scala/com/foobar/now/dao/SqlPagination.scala
+++ back/src/main/scala/com/foobar/now/dao/SqlPagination.scala
@@ -6,10 +6,10 @@ import doobie.util.query.Query0
 
 trait SqlPagination {
   def limit[A: Read](lim: Int)(q: Query0[A]): Query0[A] =
-    (q.toFragment ++ fr"LIMIT $lim").query
+    (q.toFragment ++ fr" LIMIT $lim").query
 
   def paginate[A: Read](lim: Int, offset: Int)(q: Query0[A]): Query0[A] =
-    (q.toFragment ++ fr"LIMIT $lim OFFSET $offset").query
+    (q.toFragment ++ fr" LIMIT $lim OFFSET $offset").query
 }
 
 object SqlPagination extends SqlPagination
